e308fa93951e5e72c9509ae56b7288e04098f32a
Veta <vetarara@yandex.ru>
2026-07-12 23:49:23 +0300

    create repository

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+// function App() {
+//   return (
+//     <>
+//       <div>
+//         <h1>Rick and Morty</h1>
+//       </div>
+//     </>
+//   )
+// }
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Rick and Morty</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+// создаёт корень реакт-приложения
+// StrictMode - специальная обёртка, которая помогает находить потенциальные проблемы во время разработки
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
